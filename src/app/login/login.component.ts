6117df434890f30e5354197785e8394b7b319cd6
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-12 13:18:33 +0200

    Add login request

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
+  public email: string = '';
+  public password: string = '';
+  public errorMessage: string = '';
 
+
+  constructor(private readonly http: HttpClient, private readonly router: Router) { }
+
+  onSubmit() {
+    this.http.post<{ token: string }>('/api/auth/login', { email: this.email, password: this.password })
+      .subscribe({
+        next: (response) => {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/']);
+        },
+        error: () => {
+          this.errorMessage = 'Email or password is incorrect';
+        }
+      });
+  }
 }
