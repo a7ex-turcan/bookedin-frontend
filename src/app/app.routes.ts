dbbc52fbdb3b42b903843d98b4cda7eaa72543ad
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-12 13:07:02 +0200

    Add guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {authGuard} from './auth.guard';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [authGuard],
+    component: AppComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,15 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+
+  const isLoggedIn = !!localStorage.getItem('user'); // Replace with actual authentication check
+
+  if (!isLoggedIn) {
+    router?.navigate(['/login']);
+    return false;
+  }
+
+  return true;
+};
