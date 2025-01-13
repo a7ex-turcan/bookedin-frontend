5da85a58694ad9141bca4c9201fe3233cbaa7591
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-13 18:36:29 +0200

    Add general layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {authGuard} from './auth.guard';
 import {AppComponent} from './app.component';
+import {RootComponent} from './root/root.component';
 
 export const routes: Routes = [
   {
     path: '',
     canActivate: [authGuard],
-    component: AppComponent
+    component: AppComponent,
+    children: [
+      {path: '', component: RootComponent},
+    ]
   },
   {
     path: 'login',
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -4,7 +4,7 @@ import {inject} from '@angular/core';
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
 
-  const isLoggedIn = !!localStorage.getItem('user'); // Replace with actual authentication check
+  const isLoggedIn = !!localStorage.getItem('token'); // Replace with actual authentication check
 
   if (!isLoggedIn) {
     router?.navigate(['/login']);
