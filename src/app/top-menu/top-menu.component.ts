165680e70b651d4eb7ffe371afe817249cc646f2
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-14 12:50:51 +0200

    Add image behind the avatar in the top menu

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import {AvatarComponent} from '../../shared/avatar/avatar.component';
+import { Component, Input } from '@angular/core';
+import { AvatarComponent } from '../../shared/avatar/avatar.component';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-top-menu',
   imports: [
-    AvatarComponent
+    AvatarComponent,
+    NgStyle
   ],
   templateUrl: './top-menu.component.html',
   styleUrl: './top-menu.component.sass'
 })
 export class TopMenuComponent {
-
+  @Input() backgroundImageUrl: string = '';
 }
