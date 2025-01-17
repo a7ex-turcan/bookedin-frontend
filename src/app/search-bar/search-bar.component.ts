eccbdfb4208c80424b62a481d7c4c0a171104245
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-17 18:02:02 +0200

    Refactor the search bar request making

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,13 @@
 // src/app/search-bar/search-bar.component.ts
-import { Component, HostListener, ElementRef } from '@angular/core';
-import { SearchResult } from '../../shared/search-result/search-result';
-import { SearchResultComponent } from '../../shared/search-result/search-result.component';
-import { NgForOf, NgIf } from '@angular/common';
-import { BookService } from '../../core/services/book.service';
-import { Book } from '../../core/books/book.model';
+import {Component, HostListener, ElementRef, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {SearchResult} from '../../shared/search-result/search-result';
+import {SearchResultComponent} from '../../shared/search-result/search-result.component';
+import {NgForOf, NgIf} from '@angular/common';
+import {BookService} from '../../core/services/book.service';
+import {Book} from '../../core/books/book.model';
+import {Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
@@ -12,16 +15,35 @@ import { Book } from '../../core/books/book.model';
   imports: [
     SearchResultComponent,
     NgForOf,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   styleUrls: ['./search-bar.component.sass']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit, OnChanges {
   items: SearchResult[] = [];
   showResults = false;
-  private debounceTimeout: any;
+  private searchSubject = new Subject<string>();
 
-  constructor(private eRef: ElementRef, private bookService: BookService) {}
+  public searchQuery?: string;
+
+  constructor(private eRef: ElementRef, private bookService: BookService) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    this.searchSubject.pipe(
+      debounceTime(300), // wait for 300ms pause in events
+      distinctUntilChanged(), // only emit if value is different from previous value
+      switchMap(query => this.bookService.search(query)) // switch to new search observable
+    ).subscribe(books => {
+      this.items = books.map(book => this.mapBookToSearchResult(book));
+      this.showResults = this.items.length > 0;
+    });
+  }
 
   @HostListener('document:click', ['$event'])
   clickout(event: Event) {
@@ -30,25 +52,21 @@ export class SearchBarComponent {
     }
   }
 
-  onSearch(event: Event) {
-    const query = (event.target as HTMLInputElement).value;
-    clearTimeout(this.debounceTimeout);
-
-    this.debounceTimeout = setTimeout(() => {
-      this.bookService.search(query, 7).subscribe((books: Book[]) => {
-        this.items = books.map(book => ({
-          imageUrl: 'book-placeholder.png',
-          title: book.title,
-          subTitle: book.author
-        }));
-        this.showResults = this.items.length > 0;
-      });
-    }, 300); // 300ms debounce time
-  }
-
   clearSearch(input: HTMLInputElement) {
     input.value = '';
     this.showResults = false;
     this.items = [];
+  }
+
+  private mapBookToSearchResult(book: Book): SearchResult {
+    return {
+      imageUrl: "book-placeholder.png",
+      title: book.title,
+      subTitle: book.author
+    };
+  }
+
+  onSearch($event: any) {
+    this.searchSubject.next($event);
   }
 }
