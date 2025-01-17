1ae2b8175176c8fd5628d0cef7e0f4fe75a3fdfe
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-17 18:53:34 +0200

    Show flyout when loading

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,13 +1,12 @@
-// src/app/search-bar/search-bar.component.ts
-import {Component, HostListener, ElementRef, OnInit, OnChanges, SimpleChanges} from '@angular/core';
-import {SearchResult} from '../../shared/search-result/search-result';
-import {SearchResultComponent} from '../../shared/search-result/search-result.component';
-import {NgForOf, NgIf} from '@angular/common';
-import {BookService} from '../../core/services/book.service';
-import {Book} from '../../core/books/book.model';
-import {Subject} from 'rxjs';
-import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
-import {FormsModule} from '@angular/forms';
+import { Component, HostListener, ElementRef, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { SearchResult } from '../../shared/search-result/search-result';
+import { SearchResultComponent } from '../../shared/search-result/search-result.component';
+import { NgForOf, NgIf } from '@angular/common';
+import { BookService } from '../../core/services/book.service';
+import { Book } from '../../core/books/book.model';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
@@ -23,12 +22,12 @@ import {FormsModule} from '@angular/forms';
 export class SearchBarComponent implements OnInit, OnChanges {
   items: SearchResult[] = [];
   showResults = false;
+  loading = false;
   private searchSubject = new Subject<string>();
 
   public searchQuery?: string;
 
-  constructor(private eRef: ElementRef, private bookService: BookService) {
-  }
+  constructor(private eRef: ElementRef, private bookService: BookService) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log(changes);
@@ -38,9 +37,13 @@ export class SearchBarComponent implements OnInit, OnChanges {
     this.searchSubject.pipe(
       debounceTime(300), // wait for 300ms pause in events
       distinctUntilChanged(), // only emit if value is different from previous value
-      switchMap(query => this.bookService.search(query)) // switch to new search observable
+      switchMap(query => {
+        this.loading = true;
+        return this.bookService.search(query);
+      }) // switch to new search observable
     ).subscribe(books => {
       this.items = books.map(book => this.mapBookToSearchResult(book));
+      this.loading = false;
       this.showResults = this.items.length > 0;
     });
   }
@@ -60,13 +63,23 @@ export class SearchBarComponent implements OnInit, OnChanges {
 
   private mapBookToSearchResult(book: Book): SearchResult {
     return {
-      imageUrl: "api/books/cover/"+book.coverId+"?size=s",
+      imageUrl: "api/books/cover/" + book.coverId + "?size=s",
       title: book.title,
       subTitle: book.author
     };
   }
 
   onSearch($event: any) {
+    this.showResults = true;
+    this.items = this.generateDummyItems();
     this.searchSubject.next($event);
+  }
+
+  private generateDummyItems(): SearchResult[] {
+    return Array(5).fill({
+      imageUrl: '',
+      title: 'Loading...',
+      subTitle: ''
+    });
   }
 }
