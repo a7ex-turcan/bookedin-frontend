b9ec04d5ec7c76c07c957a3db84240d6dc0fc174
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-17 18:49:14 +0200

    Add skeleton loading for images

--- src/shared/search-result/search-result.component.ts
+++ src/shared/search-result/search-result.component.ts
@@ -1,12 +1,23 @@
-import {Component, Input} from '@angular/core';
-import {SearchResult} from './search-result';
-
+// src/shared/search-result/search-result.component.ts
+import { Component, Input } from '@angular/core';
+import { SearchResult } from './search-result';
+import {NgIf} from '@angular/common';
+import {NgxSkeletonLoaderModule} from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
+  imports: [
+    NgIf,
+    NgxSkeletonLoaderModule
+  ],
   styleUrls: ['./search-result.component.sass']
 })
 export class SearchResultComponent {
   @Input() result!: SearchResult;
+  imageLoaded = false;
+
+  onImageLoad() {
+    this.imageLoaded = true;
+  }
 }
