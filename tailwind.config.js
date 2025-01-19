b2f0b9c1b87a63c0678dd24f29aa11664058a525
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-19 14:49:18 +0200

    Adjust the background color of the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-bg': '#f4f2ee',
+      },
+    },
   },
   plugins: [],
 }
