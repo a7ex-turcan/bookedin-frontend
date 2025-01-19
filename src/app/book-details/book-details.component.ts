f18e10ec6aa5ebf7f7ec1ad9a6a1cb9dde58bb16
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-19 12:14:41 +0200

    Add the layout and logic

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BookDetails } from '../../core/books/book-details.model';
 
 @Component({
   selector: 'app-book-details',
-  imports: [],
   templateUrl: './book-details.component.html',
-  styleUrl: './book-details.component.sass'
+  styleUrls: ['./book-details.component.sass']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  @Input() workId!: string;
+  book: BookDetails | null = null;
 
+  ngOnInit() {
+    // Dummy book data
+    this.book = {
+      authors: ['Author One', 'Author Two'],
+      title: 'Dummy Book Title',
+      coverId: 1,
+      workId: this.workId,
+      description: 'This is a dummy description of the book.',
+      subjects: ['Subject One', 'Subject Two']
+    };
+  }
 }
