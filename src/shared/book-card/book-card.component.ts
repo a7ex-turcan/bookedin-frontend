44efeab910d16070d3a107db46255c87fca9829d
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-21 11:42:39 +0200

    Reorder elements

--- src/shared/book-card/book-card.component.ts
+++ src/shared/book-card/book-card.component.ts
@@ -1,20 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { BookDetails } from '../../core/books/book-details.model';
-import {join} from '@angular/compiler-cli';
-import {AuthorsListPipe} from '../../core/pipes/author-list.pipe';
-import {NgIf} from '@angular/common';
+import { AuthorsListPipe } from '../../core/pipes/author-list.pipe';
+import { NgIf } from '@angular/common';
+import { FavoritesService } from '../../core/services/favorites.service';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.component.html',
   imports: [
     AuthorsListPipe,
-    NgIf,
-    // AuthorsListPipe,
-    // NgIf
+    NgIf
   ],
   styleUrls: ['./book-card.component.sass']
 })
 export class BookCardComponent {
   @Input() book: BookDetails | null = null;
+
+  constructor(private favoritesService: FavoritesService) {}
+
+  toggleFavorite(book: BookDetails | null) {
+
+    if (!book) return;
+
+    if (book.isFavorite) {
+      this.favoritesService.removeFavourite(book.workId).subscribe(() => {
+        if (book) book.isFavorite = false;
+      });
+    } else {
+      this.favoritesService.addFavourite(book.workId).subscribe(() => {
+        if (book) book.isFavorite = true;
+      });
+    }
+  }
 }
