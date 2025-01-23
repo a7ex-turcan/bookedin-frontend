b6fe5f97a3f12dcf83359ea10209323a0294ff7e
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-23 10:41:23 +0200

    Add loading skeleton indicators for user-favorites.component.html

--- src/app/user-favorites/user-favorites.component.ts
+++ src/app/user-favorites/user-favorites.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../core/books/book.model';
 import { BookCardComponent } from '../../shared/book-card/book-card.component';
-import { NgForOf } from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import { FavoritesService } from '../../core/services/favorites.service';
 
 @Component({
@@ -9,24 +9,26 @@ import { FavoritesService } from '../../core/services/favorites.service';
   templateUrl: './user-favorites.component.html',
   imports: [
     BookCardComponent,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   styleUrls: ['./user-favorites.component.sass']
 })
 export class UserFavoritesComponent implements OnInit {
   favoriteBooks: Book[] = [];
+  isLoading = true;
 
   constructor(private favoritesService: FavoritesService) {}
 
   ngOnInit() {
     this.favoritesService.getUserFavorites().subscribe((books: Book[]) => {
       this.favoriteBooks = books;
+      this.isLoading = false;
     });
   }
 
   onRemovedFromFavorites(book: Book) {
-    console.log(book)
-
-    this.favoriteBooks = this.favoriteBooks.filter(b => b.workId !== book.workId);
+    this.favoriteBooks = this.favoriteBooks.filter(b => b !== book);
+    this.favoritesService.removeFavourite(book.workId).subscribe();
   }
 }
