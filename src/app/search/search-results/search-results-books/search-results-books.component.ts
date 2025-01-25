2994ecd652cdc7a2e288278690185dc513934b87
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-25 14:21:19 +0200

    Add general layout of search-results-books.component.html

--- src/app/search/search-results/search-results-books/search-results-books.component.ts
+++ src/app/search/search-results/search-results-books/search-results-books.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookCardComponent} from "../../../../shared/book-card/book-card.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {Book} from '../../../../core/books/book.model';
+import {FavoritesService} from '../../../../core/services/favorites.service';
 
 @Component({
   selector: 'app-search-results-books',
-  imports: [],
+    imports: [
+        BookCardComponent,
+        NgForOf,
+        NgIf
+    ],
   templateUrl: './search-results-books.component.html',
   styleUrl: './search-results-books.component.sass'
 })
-export class SearchResultsBooksComponent {
+export class SearchResultsBooksComponent implements OnInit{
+  searchResult: Book[] = [];
+  isLoading = true;
 
+  constructor(private favoritesService: FavoritesService) {}
+
+  ngOnInit() {
+  }
+
+  onRemovedFromFavorites(book: Book) {
+    this.searchResult = this.searchResult.filter(b => b !== book);
+    this.favoritesService.removeFavourite(book.workId).subscribe();
+  }
 }
