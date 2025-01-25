6261b4bf9acfd30c1dfda7bd7f705c30526de8ae
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-25 14:32:51 +0200

    Add dummy books

--- src/app/search/search-results/search-results-books/search-results-books.component.ts
+++ src/app/search/search-results/search-results-books/search-results-books.component.ts
@@ -6,11 +6,11 @@ import {FavoritesService} from '../../../../core/services/favorites.service';
 
 @Component({
   selector: 'app-search-results-books',
-    imports: [
-        BookCardComponent,
-        NgForOf,
-        NgIf
-    ],
+  imports: [
+    BookCardComponent,
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './search-results-books.component.html',
   styleUrl: './search-results-books.component.sass'
 })
@@ -21,7 +21,13 @@ export class SearchResultsBooksComponent implements OnInit{
   constructor(private favoritesService: FavoritesService) {}
 
   ngOnInit() {
-    setTimeout(()=> {this.isLoading = false}, 500)
+    // Adding dummy books
+    this.searchResult = [
+      { workId: '1', title: 'Dummy Book 1', authors: ['Author 1'], coverId: 1, isFavorite: false },
+      { workId: '2', title: 'Dummy Book 2', authors: ['Author 2'], coverId: 2, isFavorite: false },
+      { workId: '3', title: 'Dummy Book 3', authors: ['Author 3'], coverId: 3, isFavorite: false }
+    ];
+    setTimeout(() => { this.isLoading = false }, 500);
   }
 
   onRemovedFromFavorites(book: Book) {
