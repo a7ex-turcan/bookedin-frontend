c7f51aa1e3a8239d5fc11170a29c794a38d8d406
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-28 08:45:02 +0200

    add shelf-card component with template and styles

---
+++ src/shared/shelf-card/shelf-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelf-card',
+  imports: [],
+  templateUrl: './shelf-card.component.html',
+  styleUrl: './shelf-card.component.sass'
+})
+export class ShelfCardComponent {
+
+}
