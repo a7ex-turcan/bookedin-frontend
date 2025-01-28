5b70b173cb46d808409d9eda42c495802d91572c
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-28 09:05:47 +0200

    add user-shelves component with template and styles

---
+++ src/app/user-shelves/user-shelves.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-shelves',
+  imports: [],
+  templateUrl: './user-shelves.component.html',
+  styleUrl: './user-shelves.component.sass'
+})
+export class UserShelvesComponent {
+
+}
