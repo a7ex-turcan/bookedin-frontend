2407da6e0789af79093a50de3275b23f2d66e24d
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-28 08:58:21 +0200

    add shelf-list component with template and styles

---
+++ src/shared/shelf-list/shelf-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shelf-list',
+  imports: [],
+  templateUrl: './shelf-list.component.html',
+  styleUrl: './shelf-list.component.sass'
+})
+export class ShelfListComponent {
+
+}
