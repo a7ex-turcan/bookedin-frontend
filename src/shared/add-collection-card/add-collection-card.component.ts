21ef58b13bed333f12533d6779e26db661346323
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-29 10:43:05 +0200

    add add-collection-card component with initial template and styles

---
+++ src/shared/add-collection-card/add-collection-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-collection-card',
+  imports: [],
+  templateUrl: './add-collection-card.component.html',
+  styleUrl: './add-collection-card.component.sass'
+})
+export class AddCollectionCardComponent {
+
+}
