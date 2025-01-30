a1ba84aba7d7af7ed26b9b43316b186677916beb
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-01-30 10:58:58 +0200

    refactor user-shelves component to use async pipe for loading shelves and improve data handling

--- src/app/user-shelves/user-shelves.component.ts
+++ src/app/user-shelves/user-shelves.component.ts
@@ -1,39 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { ShelfListComponent } from '../../shared/shelf-list/shelf-list.component';
-import { UserStoreService } from '../../core/services/user-store.service';
-import { UserBookCollectionService } from '../../core/services/user-book-collection.service';
-import { UserBookCollection } from '../../core/books/user-book-collection.model';
+import {Component, OnInit} from '@angular/core';
+import {ShelfListComponent} from '../../shared/shelf-list/shelf-list.component';
+import {UserStoreService} from '../../core/services/user-store.service';
+import {UserBookCollectionService} from '../../core/services/user-book-collection.service';
+import {UserBookCollection} from '../../core/books/user-book-collection.model';
+import {Observable, of} from 'rxjs';
+import {switchMap, map} from 'rxjs/operators';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-user-shelves',
   templateUrl: './user-shelves.component.html',
   imports: [
-    ShelfListComponent
+    ShelfListComponent,
+    AsyncPipe
   ],
   styleUrls: ['./user-shelves.component.sass']
 })
 export class UserShelvesComponent implements OnInit {
-  shelves: { shelfName: string, images: string[] }[] = [];
+  shelves$: Observable<{ shelfName: string, images: string[] }[]> = of([]);
 
   constructor(
     private userStoreService: UserStoreService,
     private userBookCollectionService: UserBookCollectionService
-  ) {}
+  ) {
+  }
 
   ngOnInit(): void {
-    this.userStoreService.user$.subscribe({
-      next: user => {
+    this.shelves$ = this.userStoreService.user$.pipe(
+      switchMap(user => {
         if (user && user.email) {
-          this.userBookCollectionService.getUserCollections(user.email).subscribe({
-            next: collections => this.shelves = collections.map(collection => ({
+          return this.userBookCollectionService.getUserCollections(user.email).pipe(
+            map(collections => collections.map(collection => ({
               shelfName: collection.collectionName,
               images: collection.books.map(book => 'api/books/cover/' + book.coverId + '?size=l')
-            })),
-            error: error => console.error('Error fetching user collections:', error)
-          });
+            })))
+          );
+        } else {
+          return [];
         }
-      },
-      error: error => console.error('Error fetching user:', error)
-    });
+      })
+    );
   }
 }
