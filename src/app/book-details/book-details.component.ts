b8f3ab5a5d69c7a3fda228510a06e7508ae71724
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-02-04 10:18:03 +0200

    refactor shelve component to improve item interaction and toggle functionality

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,120 +1,126 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { combineLatest, Observable, of } from 'rxjs';
-import { switchMap, map, catchError, distinctUntilChanged, shareReplay, tap } from 'rxjs/operators';
-import { BookDetails } from '../../core/books/book-details.model';
-import { BookService } from '../../core/services/book.service';
-import { FavoritesService } from '../../core/services/favorites.service';
-import { UserStoreService } from '../../core/services/user-store.service';
-import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
-import { AuthorsListPipe } from '../../core/pipes/author-list.pipe';
-import { ShelveComponent, ShelfItem } from '../../shared/shelve/shelve.component';
+      import { ActivatedRoute } from '@angular/router';
+      import { combineLatest, Observable, of } from 'rxjs';
+      import { switchMap, map, catchError, distinctUntilChanged, shareReplay, tap } from 'rxjs/operators';
+      import { BookDetails } from '../../core/books/book-details.model';
+      import { BookService } from '../../core/services/book.service';
+      import { FavoritesService } from '../../core/services/favorites.service';
+      import { UserStoreService } from '../../core/services/user-store.service';
+      import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
+      import { AuthorsListPipe } from '../../core/pipes/author-list.pipe';
+      import { ShelveComponent, ShelfItem } from '../../shared/shelve/shelve.component';
 
-@Component({
-  selector: 'app-book-details',
-  templateUrl: './book-details.component.html',
-  imports: [
-    AsyncPipe,
-    NgIf,
-    AuthorsListPipe,
-    NgForOf,
-    ShelveComponent
-  ],
-  styleUrls: ['./book-details.component.sass']
-})
-export class BookDetailsComponent implements OnInit {
-  book$: Observable<BookDetails | null> | null = null;
-  authorsList$: Observable<string> | null = null;
-  isLoading = true;
-  imageLoaded = false;
-  isFavorite = false;
-  showAllTags = false;
-  processedSubjects: string[] = [];
-  shelves$: Observable<ShelfItem[]> | null = null;
+      @Component({
+        selector: 'app-book-details',
+        templateUrl: './book-details.component.html',
+        imports: [
+          AsyncPipe,
+          NgIf,
+          AuthorsListPipe,
+          NgForOf,
+          ShelveComponent
+        ],
+        styleUrls: ['./book-details.component.sass']
+      })
+      export class BookDetailsComponent implements OnInit {
+        book$: Observable<BookDetails | null> | null = null;
+        authorsList$: Observable<string> | null = null;
+        isLoading = true;
+        imageLoaded = false;
+        isFavorite = false;
+        showAllTags = false;
+        processedSubjects: string[] = [];
+        shelves$: Observable<ShelfItem[]> | null = null;
 
-  constructor(
-    private bookService: BookService,
-    private route: ActivatedRoute,
-    private favoritesService: FavoritesService,
-    private userStoreService: UserStoreService
-  ) { }
+        constructor(
+          private bookService: BookService,
+          private route: ActivatedRoute,
+          private favoritesService: FavoritesService,
+          private userStoreService: UserStoreService
+        ) { }
 
-  ngOnInit() {
-    this.book$ = this.route.paramMap.pipe(
-      distinctUntilChanged(),
-      tap(_ => this.isLoading = true),
-      switchMap(params => {
-        const workId = params.get('workId');
-        if (workId) {
-          return this.bookService.getBookDetails(workId).pipe(
-            catchError(() => of(null))
+        ngOnInit() {
+          this.book$ = this.route.paramMap.pipe(
+            distinctUntilChanged(),
+            tap(_ => this.isLoading = true),
+            switchMap(params => {
+              const workId = params.get('workId');
+              if (workId) {
+                return this.bookService.getBookDetails(workId).pipe(
+                  catchError(() => of(null))
+                );
+              }
+              return of(null);
+            }),
+            map(bookDetails => {
+              this.isLoading = false;
+              if (bookDetails) {
+                this.isFavorite = bookDetails.isFavorite;
+                this.processedSubjects = this.getUniqueSplitSubjects(bookDetails.subjects);
+              }
+              return bookDetails;
+            }),
+            shareReplay(1)
+          );
+
+          this.authorsList$ = this.book$.pipe(
+            map(bookDetails => (bookDetails?.authors ?? []).map(author => author.name).join(', '))
+          );
+
+          this.shelves$ = combineLatest([
+            this.userStoreService.user$,
+            this.book$
+          ]).pipe(
+            map(([user, book]) => {
+              if (user && book) {
+                return user.collections.map(shelf => ({
+                  name: shelf.collectionName,
+                  isOnShelf: shelf.workIds.includes(book.workId)
+                }));
+              }
+              return [];
+            }),
+            catchError(() => of([]))
           );
         }
-        return of(null);
-      }),
-      map(bookDetails => {
-        this.isLoading = false;
-        if (bookDetails) {
-          this.isFavorite = bookDetails.isFavorite;
-          this.processedSubjects = this.getUniqueSplitSubjects(bookDetails.subjects);
+
+        toggleFavorite(bookDetails: BookDetails) {
+          if (this.isFavorite) {
+            this.isFavorite = false;
+            this.favoritesService.removeFavourite(bookDetails.workId).subscribe(() => {
+              this.isFavorite = false;
+            });
+          } else {
+            this.isFavorite = true;
+            this.favoritesService.addFavourite(bookDetails.workId).subscribe(() => {
+              this.isFavorite = true;
+            });
+          }
         }
-        return bookDetails;
-      }),
-      shareReplay(1)
-    );
 
-    this.authorsList$ = this.book$.pipe(
-      map(bookDetails => (bookDetails?.authors ?? []).map(author => author.name).join(', '))
-    );
-
-    this.shelves$ = combineLatest([
-      this.userStoreService.user$,
-      this.book$
-    ]).pipe(
-      map(([user, book]) => {
-        if (user && book) {
-          return user.collections.map(shelf => ({
-            name: shelf.collectionName,
-            isOnShelf: shelf.workIds.includes(book.workId)
-          }));
+        toggleShowAllTags() {
+          this.showAllTags = !this.showAllTags;
         }
-        return [];
-      }),
-      catchError(() => of([]))
-    );
-  }
 
-  toggleFavorite(bookDetails: BookDetails) {
-    if (this.isFavorite) {
-      this.isFavorite = false;
-      this.favoritesService.removeFavourite(bookDetails.workId).subscribe(() => {
-        this.isFavorite = false;
-      });
-    } else {
-      this.isFavorite = true;
-      this.favoritesService.addFavourite(bookDetails.workId).subscribe(() => {
-        this.isFavorite = true;
-      });
-    }
-  }
+        getUniqueSplitSubjects(subjects: string[]): string[] {
+          const uniqueSubjects = new Set<string>();
+          subjects.forEach(subject => {
+            subject.split(',').forEach(s => uniqueSubjects.add(s.trim()));
+          });
+          return Array.from(uniqueSubjects);
+        }
 
-  toggleShowAllTags() {
-    this.showAllTags = !this.showAllTags;
-  }
+        getDisplayedSubjects(): string[] {
+          return this.showAllTags ? this.processedSubjects : this.processedSubjects.slice(0, 10);
+        }
 
-  getUniqueSplitSubjects(subjects: string[]): string[] {
-    const uniqueSubjects = new Set<string>();
-    subjects.forEach(subject => {
-      subject.split(',').forEach(s => uniqueSubjects.add(s.trim()));
-    });
-    return Array.from(uniqueSubjects);
-  }
+        onBookAdded(shelf: ShelfItem, workId: string) {
+          // Handle the book added event
+          console.log(`Book with workId ${workId} added to shelf ${shelf.name}`);
+        }
 
-  getDisplayedSubjects(): string[] {
-    return this.showAllTags ? this.processedSubjects : this.processedSubjects.slice(0, 10);
-  }
-
-  onBookAdded() {
-
-  }
-}
+        onBookRemoved(shelf: ShelfItem, workId: string) {
+          // Handle the book removed event
+          console.log(`Book with workId ${workId} removed from shelf ${shelf.name}`);
+        }
+      }
