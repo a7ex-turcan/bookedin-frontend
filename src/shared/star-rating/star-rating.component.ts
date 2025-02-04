b7287198c7c6b6e0595e708a5768b9b4965b3792
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-02-04 19:02:41 +0200

    enhance star-rating component with hover functionality and dynamic styling

--- src/shared/star-rating/star-rating.component.ts
+++ src/shared/star-rating/star-rating.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
-import {NgClass, NgForOf} from '@angular/common';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { NgClass, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
   imports: [
-    NgForOf
+    NgForOf,
+    NgClass
   ],
   styleUrls: ['./star-rating.component.sass']
 })
@@ -13,4 +14,13 @@ export class StarRatingComponent {
   @Input() rating: number = 0;
   @Output() ratingChange: EventEmitter<number> = new EventEmitter<number>();
   stars: number[] = [1, 2, 3, 4, 5];
+  hoverIndex: number = -1;
+
+  setHover(index: number): void {
+    this.hoverIndex = index;
+  }
+
+  clearHover(): void {
+    this.hoverIndex = -1;
+  }
 }
