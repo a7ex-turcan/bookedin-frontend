1e76cb8765d3f365560c002d2a54b301c186db38
Alex Turcan <alexandru.turcan@codefactorygroup.com>
2025-02-06 09:12:44 +0200

    refactor side and top menu components to use user data for avatar display

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,20 +1,32 @@
 import { Component, Input } from '@angular/core';
-import {AvatarComponent} from '../../shared/avatar/avatar.component';
-import {NgIf, NgStyle} from '@angular/common';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { AvatarComponent } from '../../shared/avatar/avatar.component';
+import { CommonModule, NgIf, NgStyle } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserStoreService } from '../../core/services/user-store.service';
+import { Observable } from 'rxjs';
+import { User } from '../../core/users/user.model';
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
+  standalone: true,
   imports: [
     AvatarComponent,
     NgStyle,
     NgIf,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    CommonModule
   ],
   styleUrls: ['./side-menu.component.sass']
 })
 export class SideMenuComponent {
+  
   @Input() backgroundImageUrl: string | null = null;
+
+  user$: Observable<User | null>;
+
+  constructor(private userStore: UserStoreService) {
+    this.user$ = this.userStore.user$;
+  }
 }
--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,19 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { AvatarComponent } from '../../shared/avatar/avatar.component';
-import {NgStyle} from '@angular/common';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { CommonModule, NgStyle } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserStoreService } from '../../core/services/user-store.service';
+import { User } from '../../core/users/user.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-top-menu',
+  standalone: true,
   imports: [
     AvatarComponent,
     NgStyle,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    CommonModule
   ],
   templateUrl: './top-menu.component.html',
-  styleUrl: './top-menu.component.sass'
+  styleUrls: ['./top-menu.component.sass']
 })
 export class TopMenuComponent {
   @Input() backgroundImageUrl: string = '';
+
+  user$: Observable<User | null>;
+
+  constructor(private userStore: UserStoreService) {
+    this.user$ = this.userStore.user$;
+  }
 }
